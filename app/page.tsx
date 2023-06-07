2664e142e491e7d393214afb3e336174eadc95f7
patricioreguera <reguerapatriciohernan@gmail.com>
2023-06-07 19:34:58 +0200

    prueba map

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@ import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 
 const Home = async () => {
-	//const projects = await getProjects();
+	const projects = await getProjects();
 	// ya con el resultado de la llamada se puede hacer el .map
-	return <></>;
+	return (
+		<>
+			<h1>Hello</h1>
+			{projects?.map((project: any) => (
+				<h1 key={project._id}>{project.name}</h1>
+			))}
+		</>
+	);
 };
+
 export default Home;
 export const revalidate = 60; // revalidate this page every 60 seconds
